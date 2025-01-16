063155537e359adc8b5614cd5b8a1e1cdc868f44
theof <theojfagen@gmail.com>
2025-01-16 11:32:35 +0800

    add some constants and instructions to find others

--- src/main/java/raidzero/robot/Constants.java
+++ src/main/java/raidzero/robot/Constants.java
@@ -8,29 +8,40 @@ public class Constants {
     public class TelescopingArm {
         public class Telescope {
             public static final int MOTOR_ID = 10;
-            //* NUMBERS IN ROTATIONS RIGHT NOW */
-            public static final double CONVERSION_FACTOR = 1.0;
+            public static final double PULLEY_RADIUS_M = 0.0243205;
+            public static final double PLANETARY_GEAR_RATIO = 1 / 9;
 
-            //* NUMBERS IN ROTATIONS RIGHT NOW */
-            public static final double MAX_LENGTH_M = 2.0;
-            public static final double MIN_LENGTH_M = 0.0; 
+            public static final double GROUND_OFFSET_M = 0.0; // TODO: Find this
+            public static final double TIP_OFFSET_M = 0.0; // TODO: Find this
 
-            //* TEMP */
-            public static final double HOME_POSITION_ROTATIONS = 1.0;
-            public static final double TOP_POSITION_ROTATION = 70.0;
+            public static final double PULLEY_TO_TIP_RATIO = 1 / 2;
+            // * Mutliply by num rotations to get meters
+            public static final double CONVERSION_FACTOR = PULLEY_RADIUS_M * (1 / (2 * Math.PI) * PLANETARY_GEAR_RATIO) *
+                PULLEY_TO_TIP_RATIO;
 
-            //TODO: find these
+            // TODO: find these
+            public static final double MAX_LENGTH_M = 2.0 * CONVERSION_FACTOR;
+            public static final double MIN_LENGTH_M = 0.0;
+
+            // TODO: find these
+            /*
+             * To find ks: turn bot so gravity does not pull in direction of motor movement (probably on it's side), then slowly increase voltage until arm moves. That is the number
+             * To find kg: point arm straight up, do same thing, subtract ks.
+             * To find kv: point arm up, increase voltage until 1 rps. subtract kg and ks
+             * to find ks: point arm up, increase voltage until 1 rps/s. subtract kg and ks.
+             */
             public static final double KS = 0.0;
+            public static final double KG = 0.0;
             public static final double KV = 0.0;
-            public static final double KG = 4.0;
             public static final double KA = 0.0;
-            public static final double KP = 4.0;
+
+            public static final double KP = 0.0;
             public static final double KI = 0.0;
             public static final double KD = 0.0;
 
-            //TODO: Find these
-            public static final double CRUISE_VELOCITY = 110.0;
-            public static final double ACCELERATION = 250.0;
+            // TODO: Find these
+            public static final double CRUISE_VELOCITY = 0.0;
+            public static final double ACCELERATION = 0.0;
             public static final double JERK = 0.0;
 
             // 2cm of tolerance
@@ -62,5 +73,12 @@ public class Constants {
 
             public static final double ROLLER_SPEED = 0.2; // 2%
         }
+
+        // TODO: find these
+        public static final double[] L4_SCORING_POS_M = { 0.0, 0.0 };
+        public static final double[] L3_SCORING_POS_M = { 0.0, 0.0 };
+        public static final double[] L2_SCORING_POS_M = { 0.0, 0.0 };
+        public static final double[] L1_SCORING_POS_M = { 0.0, 0.0 };
+        public static final double[] INTAKE_POS_M = { 0.0, 0.0 };
     }
 }
