25e0bc813dcf76b3b437d6ba0b45968056bd78d6
Isabella Lin <59080145+i-hdk@users.noreply.github.com>
2025-01-20 16:57:53 +0800

    remove unnecessary imports

--- src/main/java/raidzero/robot/subsystems/AlgaeIntake.java
+++ src/main/java/raidzero/robot/subsystems/AlgaeIntake.java
@@ -1,18 +1,11 @@
 package raidzero.robot.subsystems;
 
-import java.util.function.BooleanSupplier;
-
-import com.ctre.phoenix6.Orchestra;
-import com.ctre.phoenix6.configs.MotionMagicConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.controls.PositionTorqueCurrentFOC;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.ForwardLimitTypeValue;
 import com.ctre.phoenix6.signals.GravityTypeValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
-import com.ctre.phoenix6.signals.ReverseLimitTypeValue;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
