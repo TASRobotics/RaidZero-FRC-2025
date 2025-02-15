4a75ff42740d411b623c25e0f5e0fd7e86fbf367
0x5b62656e5d <dev.benjamink@gmail.com>
2025-02-15 20:51:36 +0800

    Add waypoints for OTF in Constants.java

--- src/main/java/raidzero/robot/Constants.java
+++ src/main/java/raidzero/robot/Constants.java
@@ -1,9 +1,42 @@
 package raidzero.robot;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+
 public class Constants {
 
     public class Swerve {
         public static final double STICK_DEADBAND = 0.2;
+
+        public static enum REEFS {
+            LEFT, RIGHT
+        }
+
+        public static final List<Pose2d> OTF_WAYPOINTS = new ArrayList<Pose2d>(List.of(
+            new Pose2d(1.10, 0.98, Rotation2d.fromDegrees(55)), // 12 Station
+            new Pose2d(1.10, 7.07, Rotation2d.fromDegrees(-55)) // 13 Station
+        ));
+
+        public static final List<Pose2d> LEFT_REEF_WAYPOINTS = new ArrayList<Pose2d>(List.of(
+            new Pose2d(3.70, 3.16, Rotation2d.fromDegrees(60)), // 17 Left
+            new Pose2d(3.30, 3.85, Rotation2d.fromDegrees(0)), // 18 Left
+            new Pose2d(4.05, 5.1, Rotation2d.fromDegrees(300)), // 19 Left
+            new Pose2d(5.2619, 4.99953, Rotation2d.fromDegrees(240)), // 20 Left
+            new Pose2d(5.70, 3.88, Rotation2d.fromDegrees(180)), // 21 Left
+            new Pose2d(4.9419, 2.88547, Rotation2d.fromDegrees(120)) // 22 Left
+        ));
+
+        public static final List<Pose2d> RIGHT_REEF_WAYPOINTS = new ArrayList<Pose2d>(List.of(
+            new Pose2d(4.05, 2.95, Rotation2d.fromDegrees(60)), // 17 Right
+            new Pose2d(3.30, 4.15, Rotation2d.fromDegrees(0)),  // 18 Right
+            new Pose2d(3.70, 4.89, Rotation2d.fromDegrees(300)), // 19 Right
+            new Pose2d(4.9419, 5.16453, Rotation2d.fromDegrees(240)), // 20 Right
+            new Pose2d(5.70, 4.20, Rotation2d.fromDegrees(180)), // 21 Right
+            new Pose2d(5.2619, 3.05047, Rotation2d.fromDegrees(120))  // 22 Right
+        ));
     }
 
     public class TelescopingArm {
