272715a85940d274add50d5ac7864dd838d4269c
Ben <dev.benjamink@gmail.com>
2025-02-17 16:18:41 +0800

    Fix "bag of rocks"

--- src/main/java/raidzero/robot/subsystems/drivetrain/TunerConstants.java
+++ src/main/java/raidzero/robot/subsystems/drivetrain/TunerConstants.java
@@ -22,14 +22,13 @@ public class TunerConstants {
     // The steer motor uses any SwerveModule.SteerRequestType control request with the
     // output type specified by SwerveModuleConstants.SteerMotorClosedLoopOutput
     private static final Slot0Configs steerGains = new Slot0Configs()
-        .withKP(43.778).withKI(0.0).withKD(2.0975)
-        .withKS(0.2796).withKV(1.0).withKA(0.049918)
-        .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseVelocitySign);
+        .withKP(100).withKD(0.5)
+        .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign);
     // When using closed-loop control, the drive motor uses the control
     // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
     private static final Slot0Configs driveGains = new Slot0Configs()
-        .withKP(0.18264).withKI(0).withKD(0)
-        .withKS(0.15924).withKV(0.12426).withKA(0.016122);
+        .withKP(100).withKD(0.5)
+        .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign);
 
     // The closed-loop output type to use for the steer motors;
     // This affects the PID/FF gains for the steer motors
