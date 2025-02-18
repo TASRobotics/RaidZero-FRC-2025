ca7639d196009106bc5610292a9ac9d3b36d97a4
0x5b62656e5d <dev.benjamink@gmail.com>
2025-02-18 11:45:35 +0800

    Fix OTF pose initialization issue

--- src/main/java/raidzero/robot/subsystems/drivetrain/Swerve.java
+++ src/main/java/raidzero/robot/subsystems/drivetrain/Swerve.java
@@ -34,6 +34,7 @@ import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import raidzero.robot.subsystems.drivetrain.TunerConstants.TunerSwerveDrivetrain;
@@ -290,27 +291,21 @@ public class Swerve extends TunerSwerveDrivetrain implements Subsystem {
      * @return The command to move to the reef
      */
     public Command pathToReef(Constants.Swerve.REEFS reef) {
-        if (reef == Constants.Swerve.REEFS.LEFT) {
-            return AutoBuilder.pathfindToPose(
-                this.getState().Pose.nearest(Constants.Swerve.LEFT_REEF_WAYPOINTS),
+        return run(() -> {
+            AutoBuilder.pathfindToPose(
+                this.getState().Pose.nearest(
+                (reef == Constants.Swerve.REEFS.LEFT)
+                    ? Constants.Swerve.LEFT_REEF_WAYPOINTS
+                    : Constants.Swerve.RIGHT_REEF_WAYPOINTS
+                ),
                 new PathConstraints(
                     1.0,
                     3.0,
                     Units.degreesToRadians(540),
                     Units.degreesToRadians(720)
                 )
-            );
-        } else {
-            return AutoBuilder.pathfindToPose(
-                this.getState().Pose.nearest(Constants.Swerve.RIGHT_REEF_WAYPOINTS),
-                new PathConstraints(
-                    1.0,
-                    3.0,
-                    Units.degreesToRadians(540),
-                    Units.degreesToRadians(720)
-                )
-            );
-        }
+            ).schedule();
+        });
     }
 
     /**
