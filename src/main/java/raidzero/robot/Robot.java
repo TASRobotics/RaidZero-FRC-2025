a8fa0e3fb99efa93fb72a3ba7322b9bb7d49466a
0x5b62656e5d <dev.benjamink@gmail.com>
2025-02-24 21:02:13 +0800

    Format code

--- src/main/java/raidzero/robot/Robot.java
+++ src/main/java/raidzero/robot/Robot.java
@@ -10,68 +10,68 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import au.grapplerobotics.CanBridge;
 
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
+	private Command m_autonomousCommand;
 
-  private final RobotContainer m_robotContainer;
+	private final RobotContainer m_robotContainer;
 
-  public Robot() {
-    m_robotContainer = new RobotContainer();
-    CanBridge.runTCP();
-  }
+	public Robot() {
+		m_robotContainer = new RobotContainer();
+		CanBridge.runTCP();
+	}
 
-  @Override
-  public void robotPeriodic() {
-    CommandScheduler.getInstance().run();
-  }
+	@Override
+	public void robotPeriodic() {
+		CommandScheduler.getInstance().run();
+	}
 
-  @Override
-  public void disabledInit() {}
+	@Override
+	public void disabledInit() {}
 
-  @Override
-  public void disabledPeriodic() {}
+	@Override
+	public void disabledPeriodic() {}
 
-  @Override
-  public void disabledExit() {}
+	@Override
+	public void disabledExit() {}
 
-  @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+	@Override
+	public void autonomousInit() {
+		m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
-  }
+		if (m_autonomousCommand != null) {
+			m_autonomousCommand.schedule();
+		}
+	}
 
-  @Override
-  public void autonomousPeriodic() {}
+	@Override
+	public void autonomousPeriodic() {}
 
-  @Override
-  public void autonomousExit() {}
+	@Override
+	public void autonomousExit() {}
 
-  @Override
-  public void teleopInit() {
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
-  }
+	@Override
+	public void teleopInit() {
+		if (m_autonomousCommand != null) {
+			m_autonomousCommand.cancel();
+		}
+	}
 
-  @Override
-  public void teleopPeriodic() {}
+	@Override
+	public void teleopPeriodic() {}
 
-  @Override
-  public void teleopExit() {}
+	@Override
+	public void teleopExit() {}
 
-  @Override
-  public void testInit() {
-    CommandScheduler.getInstance().cancelAll();
-  }
+	@Override
+	public void testInit() {
+		CommandScheduler.getInstance().cancelAll();
+	}
 
-  @Override
-  public void testPeriodic() {}
+	@Override
+	public void testPeriodic() {}
 
-  @Override
-  public void testExit() {}
+	@Override
+	public void testExit() {}
 
-  @Override
-  public void simulationPeriodic() {}
+	@Override
+	public void simulationPeriodic() {}
 }
