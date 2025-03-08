9ca07dd8bd7a9778d441abbe4911d2b182dc3d13
0x5b62656e5d <dev.benjamink@gmail.com>
2025-03-08 11:20:16 +0800

    Add match timer to SmartDashboard

--- src/main/java/raidzero/robot/Robot.java
+++ src/main/java/raidzero/robot/Robot.java
@@ -4,7 +4,9 @@
 
 package raidzero.robot;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import raidzero.robot.subsystems.LEDStrip.ArmStrip;
@@ -72,7 +74,9 @@ public class Robot extends TimedRobot {
 	}
 	
 	@Override
-	public void teleopPeriodic() {}
+	public void teleopPeriodic() {
+		SmartDashboard.putNumber("Match Time", DriverStation.getMatchTime());
+	}
 	
 	@Override
 	public void teleopExit() {
