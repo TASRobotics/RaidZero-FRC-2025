7c527ed1cc12284d249c1c9a73f96983876a56ad
0x5b62656e5d <dev.benjamink@gmail.com>
2025-03-23 10:19:20 +0800

    Add Climb Ramp Rate to Init

--- src/main/java/raidzero/robot/RobotContainer.java
+++ src/main/java/raidzero/robot/RobotContainer.java
@@ -13,7 +13,6 @@ import com.ctre.phoenix6.swerve.SwerveRequest;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
 import com.pathplanner.lib.commands.PathfindingCommand;
-
 import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
--- src/main/java/raidzero/robot/subsystems/climb/Winch.java
+++ src/main/java/raidzero/robot/subsystems/climb/Winch.java
@@ -4,6 +4,7 @@ import com.ctre.phoenix6.configs.OpenLoopRampsConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import raidzero.robot.Constants;
@@ -29,9 +30,6 @@ public class Winch extends SubsystemBase {
      * @return A {@link Command} that runs the winch at the specified speed
      */
     public Command run(double speed, boolean ramp) {
-        if(ramp)
-            winch.getConfigurator().apply(winchConfigWithRampRate());
-
         return run(() -> winch.set(speed));
     }
 
@@ -50,15 +48,11 @@ public class Winch extends SubsystemBase {
      * @return A {@link TalonFXConfiguration} for the winch
      */
     private TalonFXConfiguration winchConfiguration() {
-        TalonFXConfiguration configuration = new TalonFXConfiguration();
-
-        return configuration;
-    }
-
-    private TalonFXConfiguration winchConfigWithRampRate() {
         TalonFXConfiguration configuration = new TalonFXConfiguration()
-            .withOpenLoopRamps(new OpenLoopRampsConfigs()
-                .withDutyCycleOpenLoopRampPeriod(0.2));
+            .withOpenLoopRamps(
+                new OpenLoopRampsConfigs()
+                    .withDutyCycleOpenLoopRampPeriod(1)
+            );
 
         return configuration;
     }
