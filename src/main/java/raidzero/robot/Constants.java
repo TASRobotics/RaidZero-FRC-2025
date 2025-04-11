1358412e131e38948138d8b5b1002d969d529837
0x5b62656e5d <dev.benjamink@gmail.com>
2025-04-11 22:06:16 +0800

    Adjust L2 scoring position

--- src/main/java/raidzero/robot/Constants.java
+++ src/main/java/raidzero/robot/Constants.java
@@ -223,7 +223,7 @@ public class Constants {
             public static final double[] L4_GRAND_SLAM = { -0.2, 1.57 };
 
             public static final double[] L3_SCORING_POS_M = { -0.20, 1.57 };
-            public static final double[] L2_SCORING_POS_M = { -0.2, 0.9 };
+            public static final double[] L2_SCORING_POS_M = { -0.15, 0.9 };
             public static final double[] L1_SCORING_POS_M = { 0.0, 0.0 };
 
             public static final double[] INTAKE_POS_M = { 0.5, 0.835 };
