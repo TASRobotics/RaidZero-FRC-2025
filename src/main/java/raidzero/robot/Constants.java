4468ca48452ac53cd0233706ba720361ba9b6f1b
0x5b62656e5d <dev.benjamink@gmail.com>
2025-04-17 12:16:41 +0800

    Update arm cancoder offset

--- src/main/java/raidzero/robot/Constants.java
+++ src/main/java/raidzero/robot/Constants.java
@@ -160,7 +160,7 @@ public class Constants {
             public static final int CANCODER_ID = 11;
 
             public static final double CANCODER_GEAR_RATIO = 28.0 / 80.0;
-            public static final double CANCODER_OFFSET = -(0.325684 - (0.25 / CANCODER_GEAR_RATIO));
+            public static final double CANCODER_OFFSET = -(0.348633 - (0.25 / CANCODER_GEAR_RATIO));
             public static final double CANCODER_DISCONTINUITY_POINT = 0.5;
             public static final double SENSOR_TO_MECHANISM_RATIO = 200.0;
 
