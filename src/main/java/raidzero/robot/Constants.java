8c6080c08006d715d541ba7c1071ccf3d2a2ca2f
0x5b62656e5d <dev.benjamink@gmail.com>
2025-04-18 10:17:18 +0800

    Update arm setpoints

--- src/main/java/raidzero/robot/Constants.java
+++ src/main/java/raidzero/robot/Constants.java
@@ -187,8 +187,8 @@ public class Constants {
         }
 
         public static class Positions {
-            public static final double[] L4_SCORING_POS_M = { -0.22, 2.75 };
-            public static final double[] L4_SCORING_POS_M_BLUE = { -0.22, 2.68 };
+            public static final double[] L4_SCORING_POS_M = { -0.1692, 2.75 };
+            public static final double[] L4_SCORING_POS_M_BLUE = { -0.1692, 2.68 };
             public static final double[] L4_CHECK_POSITION = { -0.25, 2.62 };
             public static final double[] L4_GRAND_SLAM = { -0.2, 1.57 };
 
@@ -196,8 +196,8 @@ public class Constants {
             public static final double[] L2_SCORING_POS_M = { -0.15, 0.9 };
             public static final double[] L1_SCORING_POS_M = { 0.0, 0.0 };
 
-            public static final double[] INTAKE_POS_M = { 0.5, 0.829 };
-            public static final double[] INTAKE_POS_M_BLUE = { 0.5, 0.829 };
+            public static final double[] INTAKE_POS_M = { 0.5, 0.8 };
+            public static final double[] INTAKE_POS_M_BLUE = { 0.5, 0.8 };
 
             public static final double[] L3_ALGAE_POS_M = { 0.75, 1.3 };
             public static final double[] L2_ALGAE_POS_M = { 0.6, 0.7 };
