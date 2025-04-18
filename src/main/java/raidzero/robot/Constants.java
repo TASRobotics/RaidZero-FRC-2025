9ec92691d2fd1375dd53e77a53d4c4313e71933c
Theo <theojfagen@gmail.com>
2025-04-18 10:35:23 +0800

    remove algae intake

--- src/main/java/raidzero/robot/Constants.java
+++ src/main/java/raidzero/robot/Constants.java
@@ -7,43 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Constants {
-    public static class AlgaeIntake {
-        public static class Joint {
-            public static final int MOTOR_ID = 14;
-            public static final double CONVERSION_FACTOR = (54.0 / 30.0) * 9.0;
-
-            public static final double INTAKE_POSITION = 0.0;
-            public static final double HOME_POSITION = 0.3;
-
-            public static final double KS = 0.025390625;
-            public static final double KG = 0.02734375;
-            public static final double KV = 0.5555555820465088;
-            public static final double KA = 0.014000000432133675;
-
-            public static final double KP = 20.0;
-            public static final double KI = 0.0;
-            public static final double KD = 0.0;
-
-            public static final GravityTypeValue GRAVITY_TYPE = GravityTypeValue.Arm_Cosine;
-
-            public static final double CURRENT_LIMIT = 30.0;
-            public static final double SUPPLY_CURRENT_LIMIT = 40.0;
-            public static final double SUPPLY_CURRENT_LOWER_TIME = 0.0;
-
-            public static final double MOTION_MAGIC_CRUISE_VELOCITY = 0.7;
-            public static final double MOTION_MAGIC_ACCELERATION = 4.5;
-
-            public static final double CURRENT_SPIKE_THRESHOLD_AMPS = 25.0;
-        }
-
-        public static class Intake {
-            public static final int MOTOR_ID = 15;
-            public static final double CONVERSION_FACTOR = 45.0;
-
-            public static final double INTAKE_SPEED = 0.2;
-        }
-    }
-
     public static class Bindings {
         public static final int CLIMB_UP = 1;
         public static final int CLIMB_DEPLOY = 2;
